2030a639db6bc5a7118b3fd6fb1b373c6c676f88
piotrcyra <piotr.cyra@gmail.com>
2017-07-26 01:15:10 +0200

    first commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+
+public class Calculator {
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractAFromB(int a, int b) {
+        return a - b;
+    }
+}
